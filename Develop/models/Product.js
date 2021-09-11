dc6c4d1541b0130a2f9d2f269a6ba4826a99ed23
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-11 14:07:59 -0700

    added columns to category.js, product.js files

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { truncate } = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: truncate,
+      },
+      // define product_name column
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      // define price column
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+      },
+      // define stock column
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+          isNumeric: true
+        }
+      }
   },
   {
     sequelize,
