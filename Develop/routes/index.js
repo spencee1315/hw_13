f49d163d459cb76d41108e4ed6a55ad5b29580a9
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-18 15:39:13 -0700

    updated routes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// const categoryRoutes = require('./category-routes');
+// const productRoutes = require('./product-routes');
+// const tagRoutes = require('./tag-routes');
+
+// router.use('/categories', categoryRoutes);
+// router.use('/products', productRoutes);
+// router.use('/tags', tagRoutes);
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
